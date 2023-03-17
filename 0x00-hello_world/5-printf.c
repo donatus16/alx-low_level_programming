31fc6d64fc957bd23e031050daebaa93249978ac
koutoucloui <donatuskto@gmail.com>
2023-03-16 17:26:30 -0700

    alll ta

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+# inclure  < stdio.h >
+
+/* *
+* principal - Point d'entrée
+*
+* Retour : Toujours 0 (Succès)
+ */
+int  principal ( vide )
+{
+	printf ( " avec une grammaire correcte, mais le résultat est une œuvre d'art, \n " );
+	retour ( 0 );
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+# inclure  < stdio.h >
+
+/* *
+* principal - Point d'entrée
+*
+* Retour : Toujours 0 (Succès)
+ */
+int  principal ( vide )
+{
+	int un ;
+	 entier long b ;
+	 entier long long  c ;
+	caractère d ;
+	flottant f ;
+
+	printf ( " Taille d'un caractère : %lu octet(s) \n " , ( unsigned  long ) sizeof (d));
+	printf ( " Taille d'un int : %lu octet(s) \n " , ( unsigned  long ) sizeof (a));
+	printf ( " Taille d'un entier long : %lu octet(s) \n " , ( unsigned  long ) sizeof (b));
+	printf ( " Taille d'un entier long long : %lu octet(s) \n " , ( long non signé  ) sizeof (c));
+	printf ( " Taille d'un flottant : %lu octet(s) \n " , ( unsigned  long ) sizeof (f));
+	retour ( 0 );
+}
