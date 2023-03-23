e4468da3e34445f00a247e629dc9cba906efe611
koutoucloui <donatuskto@gmail.com>
2023-03-23 12:10:45 -0700

    main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
+
 /**
-*main - causes an infinite loop
-*Infinite loop avoided! \\o/\n
-*Copy this main file. Comment out (don’t delete it!) the part
-* the code that is causing the output to go into an infinite loop
+* main - causes an infinite loop
 * Return: 0
 */
+
 int main(void)
 {
-int i;
-printf("Infinite loop incoming :(\n");
-/**
-*i = 0;
-*
-*       while (i < 10)
-*        {
-*                putchar(i);
-*Infinite loop avoided! \\o/\n
-*        }
-*/
-printf("Infinite loop avoided! \\o/\n");
-return (0);
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+	/**
+	 *	i = 0;
+	 *
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
 }
--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -9,8 +9,8 @@
 */
 void positive_or_negative(int i)
 {
-if (i < 0)
-printf("%d is negative \n", i);
-else
-printf("%d is positive \n", i);
+	if (i < 0)
+		printf("%d is negative \n", i);
+	else
+		printf("%d is positive \n", i);
 }
