3256be5550905b6099ee8d6c0018ab06d2615e0b
koutoucloui <donatuskto@gmail.com>
2023-03-27 16:33:17 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _puts - imprime une chaîne sur stdout
+* @str : pointeur vers la chaîne à imprimer
+*/
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
