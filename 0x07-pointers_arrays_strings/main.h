9deea402d8084c4fe5ee83f4362e9fde3bf7aa3b
koutoucloui <donatuskto@gmail.com>
2023-04-03 13:58:25 -0700

    2-main

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -20,4 +20,5 @@ void printASCII(char *b, int start, int end);
 void simple_print_buffer(char *buffer, unsigned int size);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 #endif
