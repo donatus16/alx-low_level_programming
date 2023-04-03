af481072bc087709c18f8ddf023e8f37062f16d7
koutoucloui <donatuskto@gmail.com>
2023-04-03 06:56:44 -0700

    0-maim

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+int _putchar(char c);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+int isPrintableASCII(int n);
+void printHexes(char *b, int start, int end);
+void printASCII(char *b, int start, int end);
+void simple_print_buffer(char *buffer, unsigned int size);
+char *_memset(char *s, char b, unsigned int n);
+#endif
