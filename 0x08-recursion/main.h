7c18706bd4bb8043f79d2d74462abfde93056892
koutoucloui <donatuskto@gmail.com>
2023-04-05 18:30:48 -0700

    all

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,5 +7,12 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 int _putchar(char c);
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int _root_sqrt(int raiz, int cont);
+int is_prime_number(int n);
 
 #endif
