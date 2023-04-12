b7e373c1614adaa20e854de22e90a62fbe7f31d5
koutoucloui <donatuskto@gmail.com>
2023-04-12 11:23:23 -0700

    all

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,18 +2,19 @@
 #include <stdlib.h>
 
 /**
- * *create_array - create array of size size and assign char c 
+ * * *create_array - create array of size size and assign char c
  * @size: size of array
- * @c: char to assign 
- * Description: creat array of size size and assign char c 
+ * @c: char to assign
+ * Description: creat array of size size and assign char c
  * Return: pointer to array, NULL if fail
- * */
+ *
+*/
 char *create_array(unsigned int size, char c)
 {
 	char *ptr;
 	unsigned int i;
 
-	ptr = malloc(sizeof(char)* size);
+	ptr = malloc(sizeof(char) * size);
 	if (size == 0 || ptr == NULL)
 		return (NULL);
 
---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_tab - Prints an array of string
+ * @tab: The array to print
+ *
+ * Return: nothing
+ */
