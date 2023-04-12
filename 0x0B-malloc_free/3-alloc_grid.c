c89d34c1c3844224444a38d8cb008e0a1765a56b
koutoucloui <donatuskto@gmail.com>
2023-04-12 11:35:46 -0700

    all

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,7 +23,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	for (x = 0; x < height; x++)
 	{
-		mee[x] malloc(sizeof(int) * width);
+		mee[x] = malloc(sizeof(int) * width);
 		if (mee[x] == NULL)
 		{
 			for (; x >= 0; x--)
