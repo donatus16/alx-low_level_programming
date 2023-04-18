17f0b96bc229fc88e419db476d2ad645df5fb9a8
koutoucloui <donatuskto@gmail.com>
2023-04-17 19:45:48 -0700

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* *
+  * main - Affiche le nom du fichier du programme
+  *
+  * Retour : Toujours zéro
+  */
+int  main ( void )
+{
+	printf ( " %s \n " , __FILE__);
+	return ( 0 );
+}
