eb96baa2e09e8784fd8e58963edb472b3e0d52bf
koutoucloui <donatuskto@gmail.com>
2023-04-18 17:48:36 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,15 +14,41 @@ char *_strcpy(char *dest, char *src);
   */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *user;
+	dog_t *cutie_dog;
+	int name_l = 0, own_l = 0;
 
-	user = malloc(sizeof(dog_t));
-	if (user == NULL)
-		return (NULL);
-	user->name = name;
-	user->age = age;
-	user->owner = owner;
-	return (user);
+	if (name != NULL && owner != NULL)
+	{
+		name_l = _strlen(name) + 1;
+		own_l = _strlen(owner) + 1;
+		cutie_dog = malloc(sizeof(dog_t));
+
+		if (cutie_dog == NULL)
+			return (NULL);
+
+		cutie_dog->name = malloc(sizeof(char) * name_l);
+
+		if (cutie_dog->name == NULL)
+		{
+			free(cutie_dog);
+			return (NULL);
+		}
+
+		cutie_dog->owner = malloc(sizeof(char) * own_l);
+
+		if (cutie_dog->owner == NULL)
+		{
+			free(cutie_dog->name);
+			free(cutie_dog);
+			return (NULL);
+		}
+
+		cutie_dog->name = _strcpy(cutie_dog->name, name);
+		cutie_dog->owner = _strcpy(cutie_dog->owner, owner);
+		cutie_dog->age = age;
+	}
+
+	return (cutie_dog);
 }
 /**
   * _strlen - Returns the length of a string
